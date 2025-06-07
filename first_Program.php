0bf98a2ae69efcd51b3509997ebf769f106186da
manjeet <yadumanjeet1234@gmail.com>
2025-06-07 15:45:17 +0530

    new programs

---
+++ first_Program.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is PHP code file </title>
+</head>
+<body>
+    <center> 
+<?php 
+echo "<font color = red size = 5> Welcome to my first PHP program ! </font>";
+echo "<br> This is my PHP certification course "
+?>
+
+</center>
+
+</body>
+</html>
