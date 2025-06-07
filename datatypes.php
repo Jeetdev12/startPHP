b1ea621345692ae6596009bb9df8c022b80bf801
manjeet <yadumanjeet1234@gmail.com>
2025-06-07 16:20:57 +0530

    Datatypes

---
+++ datatypes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $a = 150;// int
+    echo $a. "<br>";
+    $b = 23.56;
+    echo $b. "<br>";
+
+    $c = "Hello world";
+    echo $c. "<br>";
+    
+    $d= false; // boolean
+    echo $d. "<br>";
+
+    $ar = array("Kanpur", "Pune", "Mumbai");//array
+    print_r($ar);
+
+    $e = null;
+    echo $e. "<br>";
+
+
+    ?>
+</body>
+</html>
